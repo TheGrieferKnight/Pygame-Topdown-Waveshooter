1232dbb462570564a9f99a3102a34ea0aab9c2b1
thegrieferknight <stephanieherfs3@gmail.com>
2024-02-19 18:45:51 +0100

    added .gitignore

--- waves.py
+++ waves.py
@@ -2,15 +2,26 @@ import pygame
 import random
 from enemy import Enemy
 
+enemy = Enemy()
+
 class Waves:
     def __init__(self):
         super().__init__()
         self.last_spawn_time = pygame.time.get_ticks()
         self.spawn_cd = 3000
         self.enemy_counter = 0
-
+        self.spawn_multiplier = 0.009
+        
     def check_difficulty(self):
-        pass
+        if difficulty == 1:
+            self.spawn_multiplier = 1.0125
+            self.health_threshhold = 30
+        elif difficulty == 2:
+            self.spawn_multiplier = 1.025
+            self.health_threshhold = 20
+        elif difficulty == 3:
+            self.spawn_multiplier = 1.05
+            self.health_threshhold = 10
 
     def spawn_enemy(self):
         x = random.randint(0, 1920)
@@ -23,5 +34,5 @@ class Waves:
             self.spawn_enemy()
             self.enemy_counter += 1
 
-            if self.enemy_counter >= 50:
-                Enemy.kill()
+            if self.enemy_counter % self.health_threshhold == 1:
+                enemy.health += 1
