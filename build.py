3b1765d3ac727d60b7ebbb7cc0fe1723e7ade1ee
thegrieferknight <stephanieherfs3@gmail.com>
2024-02-19 21:03:44 +0100

    Added scaling speed, health and damage to enemies according to difficulty

--- build.py
+++ build.py
@@ -1,6 +1,6 @@
 import cx_Freeze
 
-executables = [cx_Freeze.Executable("baller.py")]
+executables = [cx_Freeze.Executable("main.py")]
 
 cx_Freeze.setup(
     name="A bit Racey",
---
+++ main.py
@@ -0,0 +1,61 @@
+import pygame
+import pygame_menu
+from sprites import *
+from enemy import Enemy as enemy
+from pygame_menu import Theme
+from pygame_menu import sound
+from settings import *
+from main_game import *
+pygame.init()
+
+menu_font = pygame_menu.font.FONT_COMIC_NEUE
+
+# Create Pygame Window
+pygame.display.set_caption("Baller")
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+clock = pygame.time.Clock()
+
+def settings():
+    pass
+
+# Defining the main menu theme
+myimage = pygame_menu.baseimage.BaseImage(
+    image_path="assets/enviroment/background.png",
+    drawing_mode=pygame_menu.baseimage.IMAGE_MODE_REPEAT_XY,
+)
+
+main_menu_theme = pygame_menu.Theme(
+    title_offset=(WIDTH / 2 - 100, 0),
+    title_bar_style=pygame_menu.widgets.MENUBAR_STYLE_UNDERLINE,
+    title_font=menu_font,
+    background_color=myimage,
+    title_background_color=(255, 0, 0),
+    title_font_shadow=False,
+    widget_padding=25,
+)
+
+# Create the main menu
+main_menu = pygame_menu.Menu("Main Menu", WIDTH, HEIGHT, theme=main_menu_theme)
+
+# Defining the main menu sounds
+engine = sound.Sound()
+engine.set_sound(sound.SOUND_TYPE_CLICK_MOUSE, "menu_click.mp3")
+engine.set_sound(sound.SOUND_TYPE_KEY_ADDITION, "menu_click.mp3")
+engine.set_sound(sound.SOUND_TYPE_OPEN_MENU, "menu_click.mp3")
+engine.set_sound(sound.SOUND_TYPE_WIDGET_SELECTION, "menu_click.mp3")
+engine.set_sound(sound.SOUND_TYPE_OPEN_MENU, "menu_click.mp3")
+
+main_menu.set_sound(engine, recursive=True)
+
+# Adding options to the menu
+main_menu.add.button("Play", main_game)
+main_menu.add.selector(
+    "Difficulty:",
+    [("Easy", "easy"), ("Medium", "medium"), ("Hard", "hard")],
+    onchange=enemy.change_difficulty,
+)
+main_menu.add.button("Settings", settings)  # WIP
+main_menu.add.button("Quit", pygame_menu.events.EXIT)
+
+
+main_menu.mainloop(screen)
