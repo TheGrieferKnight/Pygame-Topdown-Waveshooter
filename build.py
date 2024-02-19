f4ab7b657c6573ea2c54bdc9d8f03b91568bd916
thegrieferknight <stephanieherfs3@gmail.com>
2024-02-19 20:10:22 +0100

    First release!

---
+++ build.py
@@ -0,0 +1,11 @@
+import cx_Freeze
+
+executables = [cx_Freeze.Executable("baller.py")]
+
+cx_Freeze.setup(
+    name="A bit Racey",
+    options={"build_exe": {"packages":["pygame"],
+                           "include_files":["assets/", "main_game.py", "bullet.py", "sprites.py", "enemy.py", "player.py", "settings.py", "waves.py"]}},
+    executables = executables
+
+    )
