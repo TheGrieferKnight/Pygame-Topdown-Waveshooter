a510db57a2f8ba221873017ce87d92636dbfb9d9
thegrieferknight <stephanieherfs3@gmail.com>
2024-03-08 21:02:18 +0100

    changed the name in build.py

--- build.py
+++ build.py
@@ -6,7 +6,7 @@ import cx_Freeze
 executables = [cx_Freeze.Executable("main.py")]
 
 cx_Freeze.setup(
-    name="Pygame",
+    name="SimpleWaveshooter",
     options={
         "build_exe": {
             "packages": ["pygame"],
